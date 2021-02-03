e119df010d95a663db0fa45aa9431a0551b09c35
ozayou <75507551+ozayou@users.noreply.github.com>
2021-02-03 17:53:16 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+$(function () {
+    $('.hamburger').click(function () {
+        $(this).toggleClass('active');
+
+        if ($(this).hasClass('active')) {
+            $('.globalMenuSp').addClass('active');
+        } else {
+            $('.globalMenuSp').removeClass('active');
+        }
+    });
+    $('#menu a[href]').on('click', function (event) {
+        $('.hamburger').trigger('click');
+    });
+
+    $(window).scroll(function () {
+        $('.fadein').each(function () {
+            var elemPos = $(this).offset().top;
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll > elemPos - windowHeight + 100) {
+                $(this).addClass('scrollin');
+            }
+        });
+    });
+
+});
